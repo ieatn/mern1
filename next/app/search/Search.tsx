7eb1c65182e5e95248cae0723e94c8cd6e2199b4
ieatn <ieatnubcake@gmail.com>
2023-04-19 22:13:58 -0700

    connected api client and server nextjs

--- next/app/search/Search.tsx
+++ next/app/search/Search.tsx
@@ -2,15 +2,19 @@
 
 import { useState } from "react"
 
-// if you want to use 'use client' for usestate, you will run into an error, you have to format this file like this
+// if you want to use 'use client' for usestate or onclick, you will run into an error, you have to format this file like this
 const Search = () => {
 
-    const [data, setData] = useState('hi')
+    const fetchAPI = async () => {
+        await fetch('/api/hello', {
+            method: 'POST',
+            body: JSON.stringify({msg: 'hello'})
+        })
+    }
 
     return (
        <div>
-            <p>i am the search component</p>
-            <p>{data}</p>
+           <button onClick={fetchAPI}>Click</button>
        </div>
     )
 }
