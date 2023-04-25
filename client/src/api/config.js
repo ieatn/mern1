18d2f51296b0ff2c4c302b5b9a6cb6c7878d68db
ieatn <ieatnubcake@gmail.com>
2023-04-24 17:09:03 -0700

    server deployed

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://mern-server3.onrender.com' || 'http://localhost:4000';
+const API_URL = 'https://mern-server-k3g0.onrender.com' || 'http://localhost:4000';
 
 // since only exporting one thing, need to export as default, if not, we have to import in other file like { API_URL }
 export default API_URL
